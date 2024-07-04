3db1b6aa6673648b8223f75467ed58f12a7fb9e2
younghyun <gogocha9@naver.com>
2024-07-04 10:25:21 +0900

    Controller, Service, Interface 구현

---
+++ Blazor_Portfolio_01/Blazor_Portfolio_01/Server/Controllers/GangnamguPopulationController.cs
@@ -0,0 +1,30 @@
+﻿using Blazor_Portfolio_01.Server.Interfaces;
+using Blazor_Portfolio_01.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor_Portfolio_01.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GangnamguPopulationController : ControllerBase
+    {
+        private readonly IGangnamguPopulation _IGangnamguPopulation;
+
+        // 디펜던시 인젝션에서DI하는 방법
+        // 생성자를 통한 의존성 주입
+        // 의존성 주입은 따로 공부해보자.
+        public GangnamguPopulationController(IGangnamguPopulation iGangnamguPopulation)
+        {
+            _IGangnamguPopulation = iGangnamguPopulation;
+        }
+
+        // 컨트롤러에서 라우터로 REST API를 구분하고 알맞는 인터페이스를 연결 후
+        // 인터페이스를 통해 실제로 기능 수행을 하는 서비스로 이동한다.
+        [HttpGet]
+        public IEnumerable<GangnamguPopulation> Get() 
+        {
+            return _IGangnamguPopulation.GetAllGangnamguPopulation();
+        }
+    }
+}
---
+++ Blazor_Portfolio_01/Blazor_Portfolio_01/Server/Interfaces/IGangnamguPopulation.cs
@@ -0,0 +1,13 @@
+﻿using Blazor_Portfolio_01.Shared;
+
+namespace Blazor_Portfolio_01.Server.Interfaces
+{
+    public interface IGangnamguPopulation
+    {
+        public void AddGangnamguPopulation(GangnamguPopulation population);
+        public void UpdateGangnamguPopulation(GangnamguPopulation population);
+        public void DeleteGangnamguPopulation(int id);
+        public GangnamguPopulation GetGangnamguPopulation(int id);
+        public List<GangnamguPopulation> GetAllGangnamguPopulation();
+    }
+}
